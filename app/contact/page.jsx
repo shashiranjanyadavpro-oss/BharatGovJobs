3f6187812d4d1be32620184a8d0d20a2d643fd2e
shashiranjanyadavpro-oss <shashiranjanyadavpro@gmail.com>
2026-08-15 16:55:07 +0530

    Create page.jsx

---
+++ app/contact/page.jsx
@@ -0,0 +1,6 @@
+export default function Contact(){return(
+<div className="max-w-3xl mx-auto p-6 py-10">
+<h1 className="text-3xl font-black">Contact Us</h1>
+<p className="mt-4 text-gray-600">Email: support@bharatgovjobs.pages.dev</p>
+<p className="text-gray-600">We reply within 24 hours. Jai Hind!</p>
+</div>)}
