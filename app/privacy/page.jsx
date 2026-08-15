3eb27e4589c78007731b5b951eaad67bdc03216f
shashiranjanyadavpro-oss <shashiranjanyadavpro@gmail.com>
2026-08-15 16:53:52 +0530

    Create page.jsx

---
+++ app/privacy/page.jsx
@@ -0,0 +1,5 @@
+export default function Privacy(){return(
+<div className="max-w-3xl mx-auto p-6 py-10">
+<h1 className="text-3xl font-black">Privacy Policy</h1>
+<p className="mt-4 text-gray-600">We respect your privacy. We do not collect personal data. We use Google AdSense for ads which may use cookies. All job data is from official govt websites. For any query contact us.</p>
+</div>)}
